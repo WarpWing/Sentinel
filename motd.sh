2ad478257fd1b007032fc0b53716af4acf7ad593
Ty Chermsirivatana <professornitro@gmail.com>
2020-06-16 18:51:01 -0400

    Updated MOTD to include -q instead of -nv clause

--- motd.sh
+++ motd.sh
@@ -2,7 +2,7 @@ echo Good Morning $USER | lolcat
 
 neofetch | lolcat
 
-wget -nv https://covid19.trackercli.com && cat index.html | lolcat 
+wget -q https://covid19.trackercli.com | cat index.html > motd.txt | cat motd.txt | lolcat
 
 rm index.html
  # This works for now. There is still some verbose but it's fine at the moment. Still want to combat and eradicate any trace of verbose for a clean look
