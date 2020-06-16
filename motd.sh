406d552008f0b639e2e8f0d30a23f01e840a06b2
Ty Chermsirivatana <Ty@Orion-2.local>
2020-06-16 19:18:12 -0400

    Updated Comment

--- motd.sh
+++ motd.sh
@@ -5,4 +5,4 @@ neofetch | lolcat
 wget -q https://covid19.trackercli.com | cat index.html > motd.txt | cat motd.txt | lolcat
 
 rm index.html
- # Still doesnt work but I can work with it ish. It shows up every now and then. I could pipe the entire file through lolcat which it possible. Will look into
+ # Still doesnt work but I can work with it . It shows up every now and then. I could pipe the entire file through lolcat which it possible. Will look into it in later commits
