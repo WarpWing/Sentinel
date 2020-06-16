b6d53d466c7614f7b3b046240b05443167765f3d
Ty Chermsirivatana <Ty@Orion-2.local>
2020-06-16 12:25:55 -0400

    Renamed COVID Tracker CLI's Cat file to combat any future problems with index.html :) Still needs to completely remove verbose but this fix is acceptable

--- motd.sh
+++ motd.sh
@@ -2,7 +2,7 @@ echo Good Morning $USER | lolcat
 
 neofetch | lolcat
 
-wget -nv https://covid19.trackercli.com && cat index.html | lolcat 
+wget -nv https://covid19.trackercli.com && cat cachefile212.html | lolcat 
 
-rm index.html
+rm cachefile212.html
  # wget alternative found. still shows some verbose but it's a workaround compared to curl output. I had to use the rm command to remove the index file remaining. Not a good workaround for that problem but we can rework that soon. - Past me
