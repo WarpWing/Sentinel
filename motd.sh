2d008c3b9091b7c61a55d155ee4943b3b3031051
Ty Chermsirivatana <Ty@Orion-2.local>
2020-06-16 09:40:53 -0400

    See Note on Line 6

--- motd.sh
+++ motd.sh
@@ -3,3 +3,4 @@ echo Good Morning $USER | lolcat
 neofetch | lolcat
 
 curl -L covid19.trackercli.com | lolcat 
+ # Please note that in the future, curl needs to have % Total hidden for visual purposes
