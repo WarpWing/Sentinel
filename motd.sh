d75b935937654624723574e7fae4be455a0adad7
Ty Chermsirivatana <Ty@Orion-2.local>
2020-06-16 13:12:27 -0400

    Check Comment for Reason of Commit

--- motd.sh
+++ motd.sh
@@ -5,4 +5,4 @@ neofetch | lolcat
 wget -nv https://covid19.trackercli.com && cat index.html | lolcat 
 
 rm index.html
- # wget alternative found. still shows some verbose but it's a workaround compared to curl output. I had to use the rm command to remove the index file remaining. Not a good workaround for that problem but we can rework that soon. - Past me
+ # This works for now. There is still some verbose but it's fine at the moment. Still want to combat and eradicate any trace of verbose for a clean look
