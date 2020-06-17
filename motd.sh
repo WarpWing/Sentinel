dad4394bcc3c3de138f573d2ab21201b4dc8f01c
Ty Chermsirivatana <Ty@Orion-2.local>
2020-06-17 08:58:04 -0400

    Prototype stopping function that waits before removing any cached files on the system

--- motd.sh
+++ motd.sh
@@ -4,5 +4,8 @@ neofetch | lolcat
 
 wget -q https://covid19.trackercli.com | cat index.html > motd.txt | cat motd.txt | lolcat
 
-rm index.html
+sleep 10s
+
+rm index.html 
+rm motd.txt 
  # Still doesnt work but I can work with it . It shows up every now and then. I could pipe the entire file through lolcat which it possible. Will look into it in later commits
