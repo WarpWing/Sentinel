231165e4f8cd60cf3f0826ab30599e6f23de7005
Ty Chermsirivatana <professornitro@gmail.com>
2020-06-29 12:08:32 -0400

    No need for motd.txt.

--- motd.sh
+++ motd.sh
@@ -1,6 +1,5 @@
 neofetch | lolcat
 wget -q https://covid19.trackercli.com/history/us 
 cat us | lolcat
- # Still doesnt work but I can work with it. Lolcat pipe coming soon
 figlet WELCOME BACK $USER | lolcat 
 rm us
