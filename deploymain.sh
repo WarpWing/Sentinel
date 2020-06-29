9317d5649f4bd60d27a53eb20caaf3fe4c5565cf
Ty Chermsirivatana <professornitro@gmail.com>
2020-06-29 12:34:53 -0400

    Deployment Script v2 is a WIP but I've done some debug (screw formatting) and I got it work

--- deploymain.sh
+++ deploymain.sh
@@ -6,8 +6,17 @@ read INPUT
 if [ $INPUT = y ]
 then
   echo "Starting installation and deployment of Sentinel Files"
-  sleep 3s
+  sleep 2s
 else  
   echo "Terminating...."
   sleep 2s
-fi
+fi 
+echo -n "Would you like to install Homebrew?: "
+read INPUT 
+  if [ $INPUT = y ] 
+   then 
+       echo "Installing Homebrew" 
+       sleep 2s
+   else 
+        echo "Ok, moving on"
+fi 
