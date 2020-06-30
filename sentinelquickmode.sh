86e5ddadfaa538848be49a4cc6e6f18351a602c0
Ty Chermsirivatana <professornitro@gmail.com>
2020-06-30 13:47:36 -0400

    Modded SentinelQuickMode to have a simpler touch. Removed secondary confirmation

--- sentinelquickmode.sh
+++ sentinelquickmode.sh
@@ -1,12 +1,4 @@
 #This is the Sentinel QuickMode install script. 
-echo -n "This is the Sentinel QuickMode Install. Please confirm the install by pressing Y to start or N to abort"
-read QUICKMODE 
-
-if [ $QUICKMODE = y ] 
-    then 
-        sleep 2s
-     #Start pasting commands here 
-    
-    else 
-    echo "Ok, aborting installation"
-    
+sleep 3s
+figlet Sentinel QuickMode | lolcat
+#Put commands here
