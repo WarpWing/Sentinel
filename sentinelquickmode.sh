75c6f8cf6112ff2ca754c6fb5f7e1cb73854481e
Ty Chermsirivatana <professornitro@gmail.com>
2020-06-30 12:42:08 -0400

    Updated Quickmode with a basic install script and return back to main script (prototype)

--- sentinelquickmode.sh
+++ sentinelquickmode.sh
@@ -1,1 +1,12 @@
 #This is the Sentinel QuickMode install script. 
+echo -n "This is the Sentinel QuickMode Install. Please confirm the install by pressing Y to start or N to abort"
+read QUICKMODE 
+
+if [ $QUICKMODE = y ] 
+    then 
+        sleep 2s
+     #Start pasting commands here 
+    
+    else 
+    echo "Ok, aborting installation"
+    
