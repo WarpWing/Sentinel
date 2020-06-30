97f132ad5e0cd6bc9a868beb4fc73c7a7741375f
Ty Chermsirivatana <professornitro@gmail.com>
2020-06-30 14:22:13 -0400

    Modified deploymain to call seperate scripts

--- deploymain.sh
+++ deploymain.sh
@@ -1,11 +1,5 @@
-#!/bin/bash
-echo "Please note that this script is aimed for Ubuntu 20.04 Server LTS and up "
-sleep 2s
-figlet Sentinel Server | lolcat
-sleep 1s
 echo -n "Would you like to start deploying Sentinel Server Files?: "
 read DEPLOYINPUT
-
 if [ $DEPLOYINPUT = y ]
 then
   echo "Starting installation and deployment of Sentinel Files"
@@ -15,30 +9,3 @@ else
   sleep 2s
   exit
 fi
-echo -n "Would you like to install Homebrew?: "
-read BREWINPUT 
-  if [ $BREWINPUT = y ] 
-   then 
-       echo "Installing Homebrew" 
-       /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" 
-       sleep 3s
-       echo "Homebrew has been installed! Continuing with Deployment"
-   else 
-        echo "Continuing with Installation"
-        sleep 2s
-fi 
-echo -n "Would you like the CUSTOM MOTD included?:"
-read MOTDINPUT
-    if [ $MOTDINPUT = y ] 
-     then 
-      echo "Fetching MOTD from Github"
-      wget -q https://raw.githubusercontent.com/WarpWing/Sentinel/master/motd.sh 
-      sleep 2s 
-      chmod a+x motd.sh
-      chmod -x /etc/update-motd.d/* 
-      mv motd.sh  /etc/update-motd.d/
-      echo "MOTD has been installed and automatically moved to the proper directory"
-    else 
-    echo "Continuing with Installation"
-    sleep 2s
-fi 
