44dc0cc392a7314159d22a3021b43e258ca91880
Ty Chermsirivatana <professornitro@gmail.com>
2020-07-01 14:39:05 -0400

    Beta Script

--- deploymain.sh
+++ deploymain.sh
@@ -1,13 +1,16 @@
 figlet Sentinel Server | lolcat
 sleep 3s
-echo -n "Would you like to start deploying Sentinel Server Files?: "
+echo -n "Would you like to start deploying Sentinel Server Files?: " |lolcat
 read DEPLOYINPUT
 if [ $DEPLOYINPUT = y ]
 then
-  echo "Starting installation and deployment of Sentinel Files"
+  echo "Starting installation and deployment of Sentinel Files" | lolcat
   sleep 2s
 else  
-  echo "Terminating...."
+  echo "Terminating...." | lolcat
   sleep 2s
   exit
 fi
+echo -n "Would you like to run this in QuickMode or VerboseMode"
+sleep 1s
+echo "Quickmode"
