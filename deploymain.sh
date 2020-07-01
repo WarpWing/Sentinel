465df0fc5fbca07da22510992d033b8c849b2c52
Ty Chermsirivatana <professornitro@gmail.com>
2020-07-01 18:23:10 -0400

    Added more lolcat and more script dependencies

--- deploymain.sh
+++ deploymain.sh
@@ -11,9 +11,21 @@ else
   sleep 2s
   exit
 fi
-echo "Would you like to run this in QuickMode or VerboseMode" | lolcat
-sleep 3s 
+echo "Would you like to run this in QuickMode or VerboseMode?" | lolcat
+sleep 1s 
 echo  "1. QuickMode (Automatic)" | lolcat
+sleep 1s
 echo  "2. VerboseMode (Manuel)" | lolcat
+sleep 1s
 echo -n "Please choose a method of installaton by Number:" | lolcat
 read MODEINPUT
+if [ $MODEINPUT = 1 ] 
+then 
+ echo "Switching to QuickMode installation" | lolcat
+ sleep 3s
+ #Put Quickmode script cat here
+else 
+ echo "Switching to VerboseMode installation" | lolcat
+ sleep 3s 
+ #Put Verbosemode script cat here
+ fi 
