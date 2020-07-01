0b437c9242fb23ca0c4ed978c4a657078c6867ec
Ty Chermsirivatana <professornitro@gmail.com>
2020-07-01 18:14:50 -0400

    LOLCAT GO MEOW

--- deploymain.sh
+++ deploymain.sh
@@ -11,9 +11,9 @@ else
   sleep 2s
   exit
 fi
-echo "Would you like to run this in QuickMode or VerboseMode"
-sleep 3s
-echo  "1. QuickMode (Automatic)"
-echo  "2. VerboseMode (Manuel)"
-echo -n "Please choose a method of installaton by Number:"
+echo "Would you like to run this in QuickMode or VerboseMode" | lolcat
+sleep 3s 
+echo  "1. QuickMode (Automatic)" | lolcat
+echo  "2. VerboseMode (Manuel)" | lolcat
+echo -n "Please choose a method of installaton by Number:" | lolcat
 read MODEINPUT
