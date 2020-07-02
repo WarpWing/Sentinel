0e3ed1a4753351eb543c2a206f3dbcc5d1c44e69
Ty Chermsirivatana <professornitro@gmail.com>
2020-07-02 15:24:05 -0400

    Added ALL THE LOLCAT

--- sentinelquickmode.sh
+++ sentinelquickmode.sh
@@ -1,22 +1,22 @@
 #This is the Sentinel QuickMode install script. 
 figlet Sentinel QuickMode | lolcat
 sleep 2s
-echo "Strapping in for a quick install of Sentinel"
+echo "Strapping in for a quick install of Sentinel" | lolcat
 sleep 3s
-echo "To make it clear, this will install everything including optional modules. If you are ok with this, press y or n if otherwise"
+echo "To make it clear, this will install everything including optional modules. If you are ok with this, press y or n if otherwise" | lolcat
 read CONFIRMINPUT
 
 if [ $CONFIRMINPUT = y ]
     then
     sleep 1s
-     echo "Doing Final Checks on Installation"
+     echo "Doing Final Checks on Installation" | lolcat
      sleep 2s 
      #Smush all the commands here :) 
     else 
-  echo "Terminating QuickMode Install"
+  echo "Terminating QuickMode Install" | lolcat
   sleep 2s 
   fi 
-echo -n "Would you like to go back to the main menu?"
+echo -n "Would you like to go back to the main menu?" | lolcat
 read SECINPUT 
 
 if [ $SECINPUT = y ]
@@ -25,7 +25,7 @@ if [ $SECINPUT = y ]
     sleep 3s
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/WarpWing/Sentinel/master/deploymain.sh)"
     else 
-    echo "Terminating Installation"
+    echo "Terminating Installation" | lolcat
     sleep 2s 
     exit
 fi 
