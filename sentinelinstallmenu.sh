384d4512520043a2fd7d2b59fde2fab93319dd1e
Ty Chermsirivatana <professornitro@gmail.com>
2020-07-04 13:09:59 -0400

    Updated

--- sentinelinstallmenu.sh
+++ sentinelinstallmenu.sh
@@ -1,1 +1,10 @@
 #Put Beta Install Menu Stuff here
+#!/bin/bash
+# A simple menu system
+PS3="Enter a number: "
+
+select mode in SentinelQuickMode SentinelVerboseMode Quit
+do
+    echo "Selected mode: $mode"
+    echo "Selected number: $REPLY"
+done
