735a454db16cbfadb48172ca900f4d1f6a2f2abf
Ty Chermsirivatana <professornitro@gmail.com>
2020-07-04 14:45:04 -0400

    This is adapted from example code. I am working on learning from it and reversing engineering it :)

--- sentinelinstallmenu.sh
+++ sentinelinstallmenu.sh
@@ -1,10 +1,36 @@
 #Put Beta Install Menu Stuff here
 #!/bin/bash
 # A simple menu system
-PS3="Enter a number: "
+PS3="Select the installation method: "
 
-select mode in SentinelQuickMode SentinelVerboseMode Quit
-do
-    echo "Selected mode: $mode"
-    echo "Selected number: $REPLY"
+select opt in Quick verbose quit; do
+
+  case $opt in
+    Quick)
+      read -p "Enter the first number: " n1
+      read -p "Enter the second number: " n2
+      echo "$n1 + $n2 = $(($n1+$n2))"
+      ;;
+    verbose)
+      read -p "Enter the first number: " n1
+      read -p "Enter the second number: " n2
+      echo "$n1 - $n2 = $(($n1-$n2))"
+      ;;
+    multiply)
+      read -p "Enter the first number: " n1
+      read -p "Enter the second number: " n2
+      echo "$n1 * $n2 = $(($n1*$n2))"
+      ;;
+    divide)
+      read -p "Enter the first number: " n1
+      read -p "Enter the second number: " n2
+      echo "$n1 / $n2 = $(($n1/$n2))"
+      ;;
+    quit)
+      break
+      ;;
+    *) 
+      echo "Invalid option $REPLY"
+      ;;
+  esac
 done
