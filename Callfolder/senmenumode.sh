2ae02adb536e179e2315239d27c14cfe9c30691d
Ty Chermsirivatana <professornitro@gmail.com>
2020-07-10 22:56:20 -0400

    Added * permissions to give exec auth

---
+++ Callfolder/senmenumode.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+while :
+do
+    clear
+    cat<<EOF
+    ###############################################################################
+    #    Please select a mode to install Sentinel:                                #
+    #                                                                             #
+    #  1. Quickmode: This automatically installs everything                      #
+    #  2. Verbosemode: This lets you pick what you want and dont want to install  #                                                                      
+    ###############################################################################
+EOF 
+
+echo -n "Please choose a option"
+read NUMBER 
+
+echo -n "You have chosen $NUMBER as a option!"
+
+case $NUMBER in 
+
+    1)
+    echo "Quickmode"
+     ;; 
+    2. 
+    echo "Verbosemode"
+    ;; 
+    *)
+    echo -n "$NUMBER is not a option! Please try again"
+
+esac 
