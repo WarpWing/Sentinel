36f4fddc9d1a99a46051fe7b2fd6c54fcfa939ca
Ty Chermsirivatana <professornitro@gmail.com>
2020-07-15 12:50:33 -0400

    Added more menu space

--- Callfolder/senmenumode.sh
+++ Callfolder/senmenumode.sh
@@ -6,11 +6,12 @@ do
     clear
     cat<<EOF
     ###############################################################################
-    #    Please select a mode to install Sentinel:                                #
+    #  Please select a mode to install Sentinel:                                  #
     #                                                                             #
     #  1. Quickmode: This automatically installs everything                       #
     #  2. Verbosemode: This lets you pick what you want and dont want to install  #
     #  3. Quit: This quits the menu                                               #
+    #                                                                             #
     ###############################################################################
 EOF
 sleep 2s
