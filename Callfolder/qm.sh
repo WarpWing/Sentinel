5d1ecd44cc1eef3987581bd36012790c32d131ab
Ty Chermsirivatana <professornitro@gmail.com>
2020-07-24 20:45:31 -0400

    Removed lolcat for visual reasons

--- Callfolder/qm.sh
+++ Callfolder/qm.sh
@@ -1,16 +1,16 @@
 #This is the Sentinel QuickMode install script.  
 reset
 sleep 1s
-figlet Sentinel QuickMode | lolcat
+figlet Sentinel QuickMode 
 sleep 2s
-echo "Strapping in for a quick install of Sentinel" | lolcat
+echo "Strapping in for a quick install of Sentinel" 
 sleep 3s
 echo "To make it clear, this will install everything including optional modules. If you are ok with this, press y or n if otherwise: " | lolcat
 read CONFIRMINPUT
 if [ $CONFIRMINPUT = y ]
     then
     sleep 1s
-     echo "Doing Final Checks on Installation" | lolcat
+     echo "Doing Final Checks on Installation" 
      sleep 2s 
      #Smush all the commands here :) 
      exit
