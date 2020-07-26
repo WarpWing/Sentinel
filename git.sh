b80561428f952ad442848ed89fa2f18dcb3de25f
Ty Chermsirivatana <professornitro@gmail.com>
2020-07-26 15:46:20 -0400

    Improved git.sh by not exclusively pushing to master

--- git.sh
+++ git.sh
@@ -2,6 +2,6 @@ echo "Please type the commit message: "
 read INPUT   
 git add .
 git commit -m "$INPUT"
-git push -u origin master 
+git push -u  
 reset
 echo "Git commit has been pushed and commited to the remote branch"
