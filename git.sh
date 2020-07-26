8c8fc1241e38583dfa160cebdd780ad8cd52fb31
Ty Chermsirivatana <professornitro@gmail.com>
2020-07-26 15:29:38 -0400

    testing branch

--- git.sh
+++ git.sh
@@ -4,5 +4,5 @@ git add .
 git commit -m "$INPUT"
 git push -u origin master
 git push
- //Added extra git push to account for extra branches 
+#Added extra git push to account for extra branches 
 echo "Git commit has been pushed and commited to the remote branch"
