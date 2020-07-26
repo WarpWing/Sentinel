308320761e575be07499aa3f37849043c2c7db83
Ty Chermsirivatana <professornitro@gmail.com>
2020-07-26 15:28:33 -0400

    Did more testing

--- git.sh
+++ git.sh
@@ -2,5 +2,6 @@ echo "Please type the commit message: "
 read INPUT   
 git add .
 git commit -m "$INPUT"
-git push -u origin master 
+git push -u origin master
+git push //Added extra git push to account for extra branches 
 echo "Git commit has been pushed and commited to the remote branch"
