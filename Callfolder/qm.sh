9c81050e2399cc83dcff477097d5dde920579758
Ty Chermsirivatana <professornitro@gmail.com>
2020-07-28 16:56:51 -0400

    Removed if statement bit, dont need that since the install isnt bool

--- Callfolder/qm.sh
+++ Callfolder/qm.sh
@@ -4,18 +4,4 @@ sleep 1s
 figlet Sentinel QuickMode 
 sleep 2s
 echo "Strapping in for a quick install of Sentinel" 
-sleep 3s
-echo "To make it clear, this will install everything including optional modules. If you are ok with this, press y or n if otherwise: " | lolcat
-read CONFIRMINPUT
-if [ $CONFIRMINPUT = y ]
-    then
-    sleep 1s
-     echo "Doing Final Checks on Installation" 
-     sleep 2s 
-     #Smush all the commands here :) 
-     exit
-    else 
-  echo "Terminating QuickMode Install" | lolcat
-  sleep 1s
-  reset
-  fi 
+sleep 2s
