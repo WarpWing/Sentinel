a667ebc2bfade3f7a38f703182d8a4ea97a885cf
Ty Chermsirivatana <professornitro@gmail.com>
2020-09-14 19:07:42 -0400

    Update mymotd.sh

--- Callfolder/mymotd.sh
+++ Callfolder/mymotd.sh
@@ -1,3 +1,4 @@
 reset
-sleep 5s
-echo "The only real prison is anger. The anger of knowing that when the efforts of a person are not appreciated and instead mocked by his peers. To know that he has been working endlessly for hours on end with no sign of respect. To see himself portrayed as the model of inadequacy to the public. With me leaving SkyCrypt, I am free from these chains of burden and fly free with a new state of mind" -WarpWing, Ex-Developer at SkyCrypt
+sleep 2s
+#echo "The only real prison is anger. The anger of knowing that when the efforts of a person are not appreciated and instead mocked by his peers. To know that he has been working endlessly for hours on end with no sign of respect. To see himself portrayed as the model of inadequacy to the public. With me leaving SkyCrypt, I am free from these chains of burden and fly free with a new state of mind" -WarpWing, Ex-Developer at SkyCrypt
+curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash
