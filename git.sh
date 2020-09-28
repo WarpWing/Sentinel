d201a91cd59ec624a528012f652bc5a419b39d1c
WarpWing <professornitro@gmail.com>
2020-09-28 18:06:17 -0400

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,5 +1,28 @@
-echo "Please type the commit message: "
-read INPUT   
-git add .
-git commit -S -m "$INPUT"
-git push -u -f
+#!/bin/bash
+
+# checking if I have the latest files from github
+echo "Checking for newer files online first"
+git pull
+
+# Below command will backup everything inside the project folder
+git add --all .
+
+# Give a comment to the commit if you want
+echo "####################################"
+echo "Write your commit comment!"
+echo "####################################"
+
+read input
+
+# Committing to the local repository with a message containing the time details and commit text
+
+git commit -m "$input"
+
+# Push the local files to github
+
+git push -u origin master
+
+
+echo "################################################################"
+echo "###################    Git Push Done      ######################"
+echo "################################################################"
